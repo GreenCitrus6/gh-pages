5cdcdb1bd4aa8e74440a7e6f87baac3228fa576d
GreenCitrus6 <DanielAAad01@gmail.com>
2024-12-25 14:15:37 +0900

    added homepage to package.json

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "/gh-pages",
-    assetPrefix: "/gh-pages",
+    images: {
+        unoptimized:true,
+    },
+    basePath: isProd ? "/gh-pages" : "",
+    assetPrefix: isProd ? "/gh-pages" : "",
     output: "export",
     reactStrictMode: true,
 };
