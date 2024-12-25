828bba9c57b2ece7d581864daf6c760359fc36ff
GreenCitrus6 <DanielAAad01@gmail.com>
2024-12-25 17:02:05 +0900

    added nav with links

--- src/app/header.js
+++ src/app/header.js
@@ -2,8 +2,24 @@
 export default function Header() {
     const basePath = "/gh-pages";
     return (
-        <header>
-            <h1>hiiii</h1>
+        <header className="bg-cyan-600 text-white
+        p-2 px-4
+        flex flex-row items-center justify-between"
+        >
+            <h1 className="text-3xl font-bold">
+                hiiii
+            </h1>
+
+            <nav >
+                <a className="text-2xl m-2"
+                    href="https://ttb3ar.github.io/website/">
+                    🧸
+                </a>
+                <a className="text-2xl m-2"
+                    href="https://sunnyyou3.github.io/Website">
+                    🌞
+                </a>
+            </nav>
         </header>
     );
 }
